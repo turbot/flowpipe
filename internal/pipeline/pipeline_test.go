016e1defa59990a20ac7ba01038be4664656b6d0
Subhajit Mondal <subhajit@turbot.com>
2023-07-12 20:51:18 +0530

    Update load pipeline tests

--- internal/pipeline/pipeline_test.go
+++ internal/pipeline/pipeline_test.go
@@ -10,41 +10,56 @@ import (
 	"github.com/turbot/flowpipe/pipeparser/configschema"
 )
 
-func TestLoadPipelineDir(t *testing.T) {
-	assert := assert.New(t)
-
-	pipelines, err := LoadPipelines(context.TODO(), "./test_pipelines/simple")
-	assert.Nil(err, "error found")
-
-	assert.Greater(len(pipelines), 1, "wrong number of pipelines")
-
-	if len(pipelines) > 0 {
-		assert.NotNil(pipelines["simple_http"], "pipeline not found")
-		assert.Equal("simple_http", pipelines["simple_http"].Name, "wrong pipeline name")
-
-		for _, step := range pipelines["simple_http"].Steps {
-			if step.GetName() == "my_step_1" {
-				assert.Equal(configschema.BlockTypePipelineStepHttp, step.GetType(), "wrong step type")
-				assert.Equal("http://localhost:8081", step.GetInputs()["url"], "wrong step input")
-			}
-		}
-	}
-}
-
 func TestLoadPipelineSpecificFile(t *testing.T) {
 	assert := assert.New(t)
 
-	pipelines, err := LoadPipelines(context.TODO(), "./test_pipelines/simple/simple.fp")
+	pipelines, err := LoadPipelines(context.TODO(), "./test_pipelines/simple.fp")
 	assert.Nil(err, "error found")
 
-	assert.Greater(len(pipelines), 1, "wrong number of pipelines")
+	// Check the number of pipelines loaded
+	assert.Equal(len(pipelines), 3, "pipelines are not loaded correctly")
 
-	if len(pipelines) > 0 {
-		assert.Equal(len(pipelines), 3, "pipelines are not loaded correctly")
+	// Validate individual pipelines defined in the file
 
-		for pipeline := range pipelines {
-			assert.NotNil(pipelines[pipeline], "pipeline not found")
-			assert.Equal(pipeline, pipelines[pipeline].Name, "wrong pipeline name")
+	// Pipeline 1
+	assert.NotNil(pipelines["simple_http"], "pipeline not found")
+	assert.Equal("simple_http", pipelines["simple_http"].Name, "wrong pipeline name")
+	assert.Equal(len(pipelines["simple_http"].Steps), 3, "steps are not loaded correctly")
+
+	for _, step := range pipelines["simple_http"].Steps {
+		if step.GetName() == "my_step_1" {
+			assert.Equal(configschema.BlockTypePipelineStepHttp, step.GetType(), "wrong step type")
+			assert.Equal("http://localhost:8081", step.GetInputs()["url"], "wrong step input")
+		}
+		if step.GetName() == "sleep_1" {
+			assert.Equal(configschema.BlockTypePipelineStepSleep, step.GetType(), "wrong step type")
+			assert.Equal(int64(20), step.GetInputs()["duration"], "wrong step input")
+		}
+		if step.GetName() == "send_it" {
+			assert.Equal(configschema.BlockTypePipelineStepEmail, step.GetType(), "wrong step type")
+			assert.Equal("victor@turbot.com", step.GetInputs()["to"], "wrong step input")
+		}
+	}
+
+	// Pipeline 2
+	assert.NotNil(pipelines["simple_http_2"], "pipeline not found")
+	assert.Equal("simple_http_2", pipelines["simple_http_2"].Name, "wrong pipeline name")
+	assert.Equal(len(pipelines["simple_http_2"].Steps), 1, "steps are not loaded correctly")
+	for _, step := range pipelines["simple_http_2"].Steps {
+		if step.GetName() == "my_step_1" {
+			assert.Equal(configschema.BlockTypePipelineStepHttp, step.GetType(), "wrong step type")
+			assert.Equal("http://localhost:8081", step.GetInputs()["url"], "wrong step input")
+		}
+	}
+
+	// Pipeline 3
+	assert.NotNil(pipelines["sleep_with_output"], "pipeline not found")
+	assert.Equal("sleep_with_output", pipelines["sleep_with_output"].Name, "wrong pipeline name")
+	assert.Equal(len(pipelines["sleep_with_output"].Steps), 1, "steps are not loaded correctly")
+	for _, step := range pipelines["sleep_with_output"].Steps {
+		if step.GetName() == "sleep_1" {
+			assert.Equal(configschema.BlockTypePipelineStepSleep, step.GetType(), "wrong step type")
+			assert.Equal(int64(1), step.GetInputs()["duration"], "wrong step input")
 		}
 	}
 }
@@ -52,17 +67,64 @@ func TestLoadPipelineSpecificFile(t *testing.T) {
 func TestLoadPipelineFromFileMatchesGlob(t *testing.T) {
 	assert := assert.New(t)
 
-	pipelines, err := LoadPipelines(context.TODO(), "./test_pipelines/simple/simple*.fp")
+	pipelines, err := LoadPipelines(context.TODO(), "./test_pipelines/simple*.fp")
 	assert.Nil(err, "error found")
 
-	assert.Greater(len(pipelines), 1, "wrong number of pipelines")
+	// Check the number of pipelines loaded
+	assert.Equal(len(pipelines), 4, "pipelines are not loaded correctly")
 
-	if len(pipelines) > 0 {
-		assert.Equal(len(pipelines), 4, "pipelines are not loaded correctly")
+	// Validate individual pipelines defined in the file
 
-		for pipeline := range pipelines {
-			assert.NotNil(pipelines[pipeline], "pipeline not found")
-			assert.Equal(pipeline, pipelines[pipeline].Name, "wrong pipeline name")
+	// Pipeline 1
+	assert.NotNil(pipelines["simple_http"], "pipeline not found")
+	assert.Equal("simple_http", pipelines["simple_http"].Name, "wrong pipeline name")
+	assert.Equal(len(pipelines["simple_http"].Steps), 3, "steps are not loaded correctly")
+
+	for _, step := range pipelines["simple_http"].Steps {
+		if step.GetName() == "my_step_1" {
+			assert.Equal(configschema.BlockTypePipelineStepHttp, step.GetType(), "wrong step type")
+			assert.Equal("http://localhost:8081", step.GetInputs()["url"], "wrong step input")
+		}
+		if step.GetName() == "sleep_1" {
+			assert.Equal(configschema.BlockTypePipelineStepSleep, step.GetType(), "wrong step type")
+			assert.Equal(int64(20), step.GetInputs()["duration"], "wrong step input")
+		}
+		if step.GetName() == "send_it" {
+			assert.Equal(configschema.BlockTypePipelineStepEmail, step.GetType(), "wrong step type")
+			assert.Equal("victor@turbot.com", step.GetInputs()["to"], "wrong step input")
+		}
+	}
+
+	// Pipeline 2
+	assert.NotNil(pipelines["simple_http_2"], "pipeline not found")
+	assert.Equal("simple_http_2", pipelines["simple_http_2"].Name, "wrong pipeline name")
+	assert.Equal(len(pipelines["simple_http_2"].Steps), 1, "steps are not loaded correctly")
+	for _, step := range pipelines["simple_http_2"].Steps {
+		if step.GetName() == "my_step_1" {
+			assert.Equal(configschema.BlockTypePipelineStepHttp, step.GetType(), "wrong step type")
+			assert.Equal("http://localhost:8081", step.GetInputs()["url"], "wrong step input")
+		}
+	}
+
+	// Pipeline 3
+	assert.NotNil(pipelines["sleep_with_output"], "pipeline not found")
+	assert.Equal("sleep_with_output", pipelines["sleep_with_output"].Name, "wrong pipeline name")
+	assert.Equal(len(pipelines["sleep_with_output"].Steps), 1, "steps are not loaded correctly")
+	for _, step := range pipelines["sleep_with_output"].Steps {
+		if step.GetName() == "sleep_1" {
+			assert.Equal(configschema.BlockTypePipelineStepSleep, step.GetType(), "wrong step type")
+			assert.Equal(int64(1), step.GetInputs()["duration"], "wrong step input")
+		}
+	}
+
+	// Pipeline 4
+	assert.NotNil(pipelines["simple_http_file_2"], "pipeline not found")
+	assert.Equal("simple_http_file_2", pipelines["simple_http_file_2"].Name, "wrong pipeline name")
+	assert.Equal(len(pipelines["simple_http_file_2"].Steps), 1, "steps are not loaded correctly")
+	for _, step := range pipelines["simple_http_file_2"].Steps {
+		if step.GetName() == "my_step_1" {
+			assert.Equal(configschema.BlockTypePipelineStepHttp, step.GetType(), "wrong step type")
+			assert.Equal("http://localhost:8081", step.GetInputs()["url"], "wrong step input")
 		}
 	}
 }
@@ -70,34 +132,44 @@ func TestLoadPipelineFromFileMatchesGlob(t *testing.T) {
 func TestSleepWithOutput(t *testing.T) {
 	assert := assert.New(t)
 
-	pipelines, err := LoadPipelines(context.TODO(), "./test_pipelines/sleep_with_output/simple.fp")
+	pipelines, err := LoadPipelines(context.TODO(), "./test_pipelines/sleep_with_output.fp")
 	assert.Nil(err, "error found")
 
-	assert.Equal(len(pipelines), 1, "wrong number of pipelines")
+	assert.Equal(1, len(pipelines), "wrong number of pipelines")
+	assert.Equal(1, len(pipelines["sleep_with_output"].Steps), "steps are not loaded correctly")
 
-	if len(pipelines) > 0 {
-		assert.NotNil(pipelines["sleep_with_output"], "pipeline not found")
-		assert.Equal("sleep_with_output", pipelines["sleep_with_output"].Name, "wrong pipeline name")
+	assert.NotNil(pipelines["sleep_with_output"], "pipeline not found")
+	assert.Equal("sleep_with_output", pipelines["sleep_with_output"].Name, "wrong pipeline name")
+
+	for _, step := range pipelines["sleep_with_output"].Steps {
+		if step.GetName() == "sleep_1" {
+			assert.Equal(configschema.BlockTypePipelineStepSleep, step.GetType(), "wrong step type")
+			assert.Equal(int64(1), step.GetInputs()["duration"], "wrong step input")
+		}
 	}
 }
 
 func TestLoadPipelineDepends(t *testing.T) {
 	assert := assert.New(t)
 
-	pipelines, err := LoadPipelines(context.TODO(), "./test_pipelines/depends_on/depends.fp")
+	pipelines, err := LoadPipelines(context.TODO(), "./test_pipelines/depends.fp")
 	assert.Nil(err, "error found")
 
-	assert.Greater(len(pipelines), 0, "wrong number of pipelines")
+	// Check the number of pipelines loaded
+	assert.Equal(len(pipelines), 1, "pipelines are not loaded correctly")
 
-	if len(pipelines) > 0 {
-		assert.NotNil(pipelines["http_and_sleep_depends"], "pipeline not found")
-		assert.Equal("http_and_sleep_depends", pipelines["http_and_sleep_depends"].Name, "wrong pipeline name")
+	assert.NotNil(pipelines["http_and_sleep_depends"], "pipeline not found")
+	assert.Equal("http_and_sleep_depends", pipelines["http_and_sleep_depends"].Name, "wrong pipeline name")
+	assert.Equal(len(pipelines["http_and_sleep_depends"].Steps), 2, "steps are not loaded correctly")
 
-		for _, step := range pipelines["http_and_sleep_depends"].Steps {
-			if step.GetName() == "sleep_1" {
-				assert.Equal(configschema.BlockTypePipelineStepSleep, step.GetType(), "wrong step type")
-				assert.Equal("http.http_1", step.GetDependsOn()[0], "wrong step depends on")
-			}
+	for _, step := range pipelines["http_and_sleep_depends"].Steps {
+		if step.GetName() == "http_1" {
+			assert.Equal(configschema.BlockTypePipelineStepHttp, step.GetType(), "wrong step type")
+			assert.Equal("http://api.open-notify.org/astros.json", step.GetInputs()["url"], "wrong step input")
+		}
+		if step.GetName() == "sleep_1" {
+			assert.Equal(configschema.BlockTypePipelineStepSleep, step.GetType(), "wrong step type")
+			assert.Equal("http.http_1", step.GetDependsOn()[0], "wrong step depends on")
 		}
 	}
 }
@@ -105,7 +177,7 @@ func TestLoadPipelineDepends(t *testing.T) {
 func TestLoadPipelineInvalidDepends(t *testing.T) {
 	assert := assert.New(t)
 
-	_, err := LoadPipelines(context.TODO(), "./test_pipelines/invalid_depends_on/invalid.fp")
+	_, err := LoadPipelines(context.TODO(), "./test_pipelines/invalid.fp")
 	assert.NotNil(err, "error not found")
 
 	// TODO: need to improve the error here, need more context? sub-code?
@@ -114,31 +186,64 @@ func TestLoadPipelineInvalidDepends(t *testing.T) {
 
 func TestMarshallUnmarshal(t *testing.T) {
 	assert := assert.New(t)
-	pipelines, err := LoadPipelines(context.TODO(), "./test_pipelines/simple/simple.fp")
+	pipelines, err := LoadPipelines(context.TODO(), "./test_pipelines/simple.fp")
 	assert.Nil(err, "error found")
 
-	assert.Greater(len(pipelines), 1, "wrong number of pipelines")
+	// Check the number of pipelines loaded
+	assert.Equal(len(pipelines), 3, "pipelines are not loaded correctly")
 
-	if len(pipelines) > 0 {
-		assert.NotNil(pipelines["simple_http"], "pipeline not found")
-		assert.Equal("simple_http", pipelines["simple_http"].Name, "wrong pipeline name")
+	for pipeline := range pipelines {
+		assert.NotNil(pipelines[pipeline], "pipeline not found")
 
-		data, err := json.Marshal(pipelines["simple_http"])
+		data, err := json.Marshal(pipelines[pipeline])
 		assert.Nil(err, "error found, can't marshall")
 
 		var p types.PipelineHcl
 		err = json.Unmarshal(data, &p)
 		assert.Nil(err, "error found, can't unmarshall")
 
-		found := false
-		for _, step := range pipelines["simple_http"].Steps {
-			if step.GetName() == "my_step_1" {
-				found = true
-				assert.Equal(configschema.BlockTypePipelineStepHttp, step.GetType(), "wrong step type")
-				assert.Equal("http://localhost:8081", step.GetInputs()["url"], "wrong step input")
+		if pipeline == "simple_http" {
+			assert.Equal("simple_http", p.Name, "wrong pipeline name")
+			assert.Equal(3, len(p.Steps), "steps are not loaded correctly")
+
+			for _, step := range p.Steps {
+				if step.GetName() == "my_step_1" {
+					assert.Equal(configschema.BlockTypePipelineStepHttp, step.GetType(), "wrong step type")
+					assert.Equal("http://localhost:8081", step.GetInputs()["url"], "wrong step input")
+				}
+				if step.GetName() == "sleep_1" {
+					assert.Equal(configschema.BlockTypePipelineStepSleep, step.GetType(), "wrong step type")
+					assert.Equal(int64(20), step.GetInputs()["duration"], "wrong step input")
+				}
+				if step.GetName() == "send_it" {
+					assert.Equal(configschema.BlockTypePipelineStepEmail, step.GetType(), "wrong step type")
+					assert.Equal("victor@turbot.com", step.GetInputs()["to"], "wrong step input")
+				}
 			}
 		}
-		assert.True(found, "step not found")
-	}
 
+		if pipeline == "simple_http_2" {
+			assert.Equal("simple_http_2", p.Name, "wrong pipeline name")
+			assert.Equal(1, len(p.Steps), "steps are not loaded correctly")
+
+			for _, step := range p.Steps {
+				if step.GetName() == "my_step_1" {
+					assert.Equal(configschema.BlockTypePipelineStepHttp, step.GetType(), "wrong step type")
+					assert.Equal("http://localhost:8081", step.GetInputs()["url"], "wrong step input")
+				}
+			}
+		}
+
+		if pipeline == "sleep_with_output" {
+			assert.Equal("sleep_with_output", p.Name, "wrong pipeline name")
+			assert.Equal(1, len(p.Steps), "steps are not loaded correctly")
+
+			for _, step := range p.Steps {
+				if step.GetName() == "sleep_1" {
+					assert.Equal(configschema.BlockTypePipelineStepSleep, step.GetType(), "wrong step type")
+					assert.Equal(int64(1), step.GetInputs()["duration"], "wrong step input")
+				}
+			}
+		}
+	}
 }
