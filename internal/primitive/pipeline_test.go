3e1a106dbecfb388e97b7b5716b21f5878aeee0c
Subhajit Mondal <subhajit@turbot.com>
2023-07-17 11:25:10 +0530

    Add unit tests for sleep, pipeline primitives, fix exec primitive TestExecProgramNotFound test to match a part of the output since the output depends on the terminal #7 (#14)

---
+++ internal/primitive/pipeline_test.go
@@ -0,0 +1,23 @@
+package primitive
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/turbot/flowpipe/internal/fplog"
+	"github.com/turbot/flowpipe/internal/types"
+)
+
+func TestPipelineOK(t *testing.T) {
+	ctx := context.Background()
+	ctx = fplog.ContextWithLogger(ctx)
+
+	assert := assert.New(t)
+	q := RunPipeline{}
+	input := types.Input(map[string]interface{}{"name": "my_pipeline", "args": map[string]interface{}{}})
+
+	output, err := q.Run(ctx, input)
+	assert.Nil(err)
+	assert.Equal("my_pipeline", output.Get("name").(string), "wrong pipeline name")
+}
