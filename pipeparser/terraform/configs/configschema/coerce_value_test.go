60ad632ebf505104be647d02f8cd02236a1c5493
Victor Hadianto <victor@turbot.com>
2023-07-17 04:51:49 +0000

    Fixed reference to tfdiags.

--- pipeparser/terraform/configs/configschema/coerce_value_test.go
+++ pipeparser/terraform/configs/configschema/coerce_value_test.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/zclconf/go-cty/cty"
 
-	"github.com/hashicorp/terraform/internal/tfdiags"
+	"github.com/turbot/flowpipe/pipeparser/terraform/tfdiags"
 )
 
 func TestCoerceValue(t *testing.T) {
