33d7cf6c373f1732a0b16002715395c8ba2c85a0
Subhajit Mondal <subhajit@turbot.com>
2023-07-21 15:21:07 +0530

    Add POST functionality to HTTP Primitive. Closes #19 (#22)

--- internal/primitive/http.go
+++ internal/primitive/http.go
@@ -1,9 +1,11 @@
 package primitive
 
 import (
+	"bytes"
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
 	"net/url"
@@ -14,8 +16,27 @@ import (
 	"github.com/turbot/flowpipe/internal/types"
 )
 
+const (
+	HTTPRequestGet  = "get"
+	HttpRequestPost = "post"
+
+	// HTTPRequestDefaultTimeoutMs is the default timeout for HTTP requests
+	// For now the value is hardcoded to 3000 milliseconds
+	// TODO: Make this configurable
+	HTTPRequestDefaultTimeoutMs = 3000
+)
+
 type HTTPRequest struct {
 	Input types.Input
+}
+
+type HTTPPOSTInput struct {
+	URL              string
+	RequestBody      string
+	RequestHeaders   map[string]interface{}
+	RequestTimeoutMs int
+	CaCertPem        string
+	Insecure         bool
 }
 
 func (h *HTTPRequest) ValidateInput(ctx context.Context, i types.Input) error {
@@ -25,7 +46,26 @@ func (h *HTTPRequest) ValidateInput(ctx context.Context, i types.Input) error {
 	u := i["url"].(string)
 	_, err := url.ParseRequestURI(u)
 	if err != nil {
-		return fmt.Errorf("invalid url: %s", u)
+		return fperr.BadRequestWithMessage("invalid url: " + u)
+	}
+
+	requestBody := i["body"]
+	if requestBody != nil {
+		// Try to unmarshal the request body into JSON
+		var requestBodyJSON map[string]interface{}
+		unmarshalErr := json.Unmarshal([]byte(requestBody.(string)), &requestBodyJSON)
+		if unmarshalErr != nil {
+			// If unmarshaling fails, assume it's a plain string
+			requestBodyJSON = nil
+		}
+
+		// If the request body is a JSON object
+		if requestBodyJSON != nil {
+			_, marshalErr := json.Marshal(requestBodyJSON)
+			if marshalErr != nil {
+				return fperr.BadRequestWithMessage("error marshaling request body JSON: " + marshalErr.Error())
+			}
+		}
 	}
 	return nil
 }
@@ -35,19 +75,138 @@ func (h *HTTPRequest) Run(ctx context.Context, input types.Input) (*types.StepOu
 		return nil, err
 	}
 
-	logger := fplog.Logger(ctx)
-
 	// TODO
-	// * POST and other methods
-	// * Handle server not found errors - https://steampipe.notfound/
+	// * Currently the primitive only supports GET and POST requests. Add support for other methods.
 	// * Test SSL vs non-SSL
 	// * Compare to features in https://www.tines.com/docs/actions/types/http-request#configuration-options
 
+	method := input["method"].(string)
+	inputURL := input["url"].(string)
+
+	var output *types.StepOutput
+	var err error
+	switch method {
+	case HTTPRequestGet:
+		output, err = get(ctx, inputURL)
+	case HttpRequestPost:
+		// build the input for the POST request
+		postInput, inputErr := buildHTTPPostInput(input)
+		if inputErr != nil {
+			return nil, inputErr
+		}
+
+		output, err = post(ctx, postInput)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
+
+func get(ctx context.Context, inputURL string) (*types.StepOutput, error) {
+	logger := fplog.Logger(ctx)
+
 	start := time.Now().UTC()
-	resp, err := http.Get(input["url"].(string))
+	resp, err := http.Get(inputURL)
 	finish := time.Now().UTC()
 	if err != nil {
 		logger.Error("error making request", "error", err, "response", resp)
+		return nil, err
+	}
+	defer func() {
+		if resp != nil && resp.Body != nil {
+			resp.Body.Close()
+		}
+	}()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Golang Response.Header is a map[string][]string, which is accurate
+	// but complicated for users. We map it to a simpler key-value pair
+	// approach.
+	headers := map[string]interface{}{}
+	// But, well known multi-value fields (e.g. Set-Cookie) should be maintained
+	// in array form
+	headersAsArrays := map[string]bool{"Set-Cookie": true}
+
+	for k, v := range resp.Header {
+		if headersAsArrays[k] {
+			// It's a known multi-value header
+			headers[k] = v
+		} else {
+			// Otherwise, just use the first value for simplicity
+			headers[k] = v[0]
+		}
+	}
+
+	output := types.StepOutput{
+		"status":      resp.Status,
+		"status_code": resp.StatusCode,
+		"headers":     headers,
+		"started_at":  start,
+		"finished_at": finish,
+	}
+
+	if body != nil {
+		output["body"] = string(body)
+	}
+
+	var bodyJSON interface{}
+
+	// Process the response body only if the status code is 200
+	if resp != nil && resp.StatusCode == http.StatusOK {
+		// The unmarshalling is only done if the content type is JSON,
+		// otherwise the unmashalling will fail.
+		// Hence, the body_json field will only be populated if the content type is JSON.
+		if resp.Header.Get("Content-Type") == "application/json" {
+			err = json.Unmarshal(body, &bodyJSON)
+			if err != nil {
+				logger.Error("error unmarshalling body: %s", err)
+				return nil, err
+			}
+			output["body_json"] = bodyJSON
+		}
+	}
+
+	return &output, nil
+}
+
+func post(ctx context.Context, inputParams *HTTPPOSTInput) (*types.StepOutput, error) {
+	logger := fplog.Logger(ctx)
+
+	// Create the HTTP client
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", inputParams.URL, bytes.NewBuffer([]byte(inputParams.RequestBody)))
+	if err != nil {
+		return nil, fperr.BadRequestWithMessage("Error creating request: " + err.Error())
+	}
+
+	// Set the request headers
+	for k, v := range inputParams.RequestHeaders {
+		req.Header.Set(k, v.(string))
+	}
+
+	if inputParams.CaCertPem != "" {
+		caCertPool := x509.NewCertPool()
+		caCertPool.AppendCertsFromPEM([]byte(inputParams.CaCertPem))
+
+		client.Transport = &http.Transport{
+			TLSClientConfig: &tls.Config{
+				RootCAs:            caCertPool,
+				InsecureSkipVerify: inputParams.Insecure,
+			},
+		}
+	}
+
+	start := time.Now().UTC()
+	resp, err := client.Do(req)
+	finish := time.Now().UTC()
+	if err != nil {
 		return nil, err
 	}
 	defer func() {
@@ -110,4 +269,60 @@ func (h *HTTPRequest) Run(ctx context.Context, input types.Input) (*types.StepOu
 	}
 
 	return &output, nil
+}
+
+// builsHTTPPostInput builds the HTTPPOSTInput struct from the input parameters
+func buildHTTPPostInput(input types.Input) (*HTTPPOSTInput, error) {
+	// Get the inputs from the pipeline
+	inputParams := &HTTPPOSTInput{
+		URL: input["url"].(string),
+
+		// TODO: Make it configurable
+		RequestTimeoutMs: HTTPRequestDefaultTimeoutMs,
+	}
+
+	// Set the certificate, if provided
+	if input["ca_cert_pem"] != nil {
+		inputParams.CaCertPem = input["ca_cert_pem"].(string)
+	}
+
+	// Set value for insecureSkipVerify, if provided
+	if input["insecure"] != nil {
+		inputParams.Insecure = input["insecure"].(bool)
+	}
+
+	// Set the request headers, if provided
+	requestHeaders := map[string]interface{}{}
+	if input["request_headers"] != nil {
+		requestHeaders = input["request_headers"].(map[string]interface{})
+	}
+
+	// Get the request body
+	requestBody := input["body"]
+
+	if requestBody != nil {
+		// Try to unmarshal the request body into JSON
+		var requestBodyJSON map[string]interface{}
+		unmarshalErr := json.Unmarshal([]byte(requestBody.(string)), &requestBodyJSON)
+		if unmarshalErr != nil {
+			// If unmarshaling fails, assume it's a plain string
+			requestBodyJSON = nil
+
+			// Set the request body as a string
+			inputParams.RequestBody = requestBody.(string)
+
+			// Also, set the content type header to plain text
+			requestHeaders["Content-Type"] = "text/plain"
+		}
+
+		// If the request body is a JSON object
+		if requestBodyJSON != nil {
+			// Set the JSON encoding of the request body
+			requestBodyJSONBytes, _ := json.Marshal(requestBodyJSON)
+			inputParams.RequestBody = string(requestBodyJSONBytes)
+		}
+	}
+	inputParams.RequestHeaders = requestHeaders
+
+	return inputParams, nil
 }
--- internal/primitive/http_test.go
+++ internal/primitive/http_test.go
@@ -15,7 +15,7 @@ func TestHTTPRequestOK(t *testing.T) {
 
 	assert := assert.New(t)
 	hr := HTTPRequest{}
-	input := types.Input(map[string]interface{}{"url": "https://steampipe.io/"})
+	input := types.Input(map[string]interface{}{"url": "https://steampipe.io/", "method": "get"})
 	output, err := hr.Run(ctx, input)
 	assert.Nil(err)
 	assert.Equal("200 OK", output.Get("status"))
@@ -30,7 +30,7 @@ func TestHTTPRequestJSONResponseOK(t *testing.T) {
 
 	assert := assert.New(t)
 	hr := HTTPRequest{}
-	input := types.Input(map[string]interface{}{"url": "http://api.open-notify.org/astros.json"})
+	input := types.Input(map[string]interface{}{"url": "http://api.open-notify.org/astros.json", "method": "get"})
 	output, err := hr.Run(ctx, input)
 	assert.Nil(err)
 	assert.Equal("200 OK", output.Get("status"))
@@ -45,11 +45,166 @@ func TestHTTPRequestNotFound(t *testing.T) {
 
 	assert := assert.New(t)
 	hr := HTTPRequest{}
-	input := types.Input(map[string]interface{}{"url": "https://steampipe.io/asdlkfjasdlfkjnotfound/"})
+	input := types.Input(map[string]interface{}{"url": "https://steampipe.io/asdlkfjasdlfkjnotfound/", "method": "get"})
 	output, err := hr.Run(ctx, input)
 	assert.Nil(err)
 	assert.Equal("404 Not Found", output.Get("status"))
 	assert.Equal(404, output.Get("status_code"))
 	assert.Equal("text/html; charset=utf-8", output.Get("headers").(map[string]interface{})["Content-Type"])
 	assert.Contains(output.Get("body"), "Steampipe")
+}
+
+func TestHTTPPOSTRequestOK(t *testing.T) {
+	ctx := context.Background()
+	ctx = fplog.ContextWithLogger(ctx)
+
+	assert := assert.New(t)
+	hr := HTTPRequest{}
+	input := types.Input(map[string]interface{}{
+		"url":    "https://jsonplaceholder.typicode.com/posts",
+		"method": "post",
+		"body": `{
+			"userId": 1001,
+			"it": 1001,
+			"title": "Test 1001"
+		}`,
+	})
+	output, err := hr.Run(ctx, input)
+	assert.Nil(err)
+	assert.Equal("201 Created", output.Get("status"))
+	assert.Equal(201, output.Get("status_code"))
+	assert.Equal("application/json; charset=utf-8", output.Get("headers").(map[string]interface{})["Content-Type"])
+	assert.Contains(output.Get("body"), "id")
+}
+
+func TestHTTPPOSTRequestOKWithTextBody(t *testing.T) {
+	ctx := context.Background()
+	ctx = fplog.ContextWithLogger(ctx)
+
+	assert := assert.New(t)
+	hr := HTTPRequest{}
+	input := types.Input(map[string]interface{}{
+		"url":    "https://jsonplaceholder.typicode.com/posts",
+		"method": "post",
+		"body":   "Test",
+	})
+	output, err := hr.Run(ctx, input)
+	assert.Nil(err)
+	assert.Equal("201 Created", output.Get("status"))
+	assert.Equal(201, output.Get("status_code"))
+	assert.Equal("application/json; charset=utf-8", output.Get("headers").(map[string]interface{})["Content-Type"])
+	assert.Contains(output.Get("body"), "id")
+}
+
+func TestHTTPPOSTRequestNotFound(t *testing.T) {
+	ctx := context.Background()
+	ctx = fplog.ContextWithLogger(ctx)
+
+	assert := assert.New(t)
+	hr := HTTPRequest{}
+	input := types.Input(map[string]interface{}{"url": "http://www.example.com/notfound", "method": "post"})
+	output, err := hr.Run(ctx, input)
+	assert.Nil(err)
+	assert.Equal("404 Not Found", output.Get("status"))
+	assert.Equal(404, output.Get("status_code"))
+	assert.Equal("text/html; charset=UTF-8", output.Get("headers").(map[string]interface{})["Content-Type"])
+}
+
+func TestHTTPPOSTRequestOKWithRequestHeaders(t *testing.T) {
+	ctx := context.Background()
+	ctx = fplog.ContextWithLogger(ctx)
+
+	assert := assert.New(t)
+	hr := HTTPRequest{}
+	input := types.Input(map[string]interface{}{
+		"url":    "https://jsonplaceholder.typicode.com/posts",
+		"method": "post",
+		"body": `{
+			"userId": 1001,
+			"it": 1001,
+			"title": "Test 1001"
+		}`,
+		"request_headers": map[string]interface{}{
+			"Accept":     "application/json",
+			"User-Agent": "flowpipe",
+		},
+	})
+	output, err := hr.Run(ctx, input)
+	assert.Nil(err)
+	assert.Equal("201 Created", output.Get("status"))
+	assert.Equal(201, output.Get("status_code"))
+	assert.Equal("application/json; charset=utf-8", output.Get("headers").(map[string]interface{})["Content-Type"])
+	assert.Contains(output.Get("body"), "id")
+}
+
+func TestHTTPPOSTRequestOKWithTimeout(t *testing.T) {
+	ctx := context.Background()
+	ctx = fplog.ContextWithLogger(ctx)
+
+	assert := assert.New(t)
+	hr := HTTPRequest{}
+	input := types.Input(map[string]interface{}{
+		"url":    "https://jsonplaceholder.typicode.com/posts",
+		"method": "post",
+		"body": `{
+			"userId": 1001,
+			"it": 1001,
+			"title": "Test 1001"
+		}`,
+		"request_timeout_ms": 3000,
+	})
+	output, err := hr.Run(ctx, input)
+	assert.Nil(err)
+	assert.Equal("201 Created", output.Get("status"))
+	assert.Equal(201, output.Get("status_code"))
+	assert.Equal("application/json; charset=utf-8", output.Get("headers").(map[string]interface{})["Content-Type"])
+	assert.Contains(output.Get("body"), "id")
+}
+
+func TestHTTPPOSTRequestOKWithNoVerifyCertificate(t *testing.T) {
+	ctx := context.Background()
+	ctx = fplog.ContextWithLogger(ctx)
+
+	assert := assert.New(t)
+	hr := HTTPRequest{}
+	input := types.Input(map[string]interface{}{
+		"url":    "https://jsonplaceholder.typicode.com/posts",
+		"method": "post",
+		"body": `{
+			"userId": 1001,
+			"it": 1001,
+			"title": "Test 1001"
+		}`,
+		"insecure":    true,
+		"ca_cert_pem": "test",
+	})
+	output, err := hr.Run(ctx, input)
+	assert.Nil(err)
+	assert.Equal("201 Created", output.Get("status"))
+	assert.Equal(201, output.Get("status_code"))
+	assert.Equal("application/json; charset=utf-8", output.Get("headers").(map[string]interface{})["Content-Type"])
+	assert.Contains(output.Get("body"), "id")
+}
+
+func TestHTTPPOSTRequestWithVerifyCertificate(t *testing.T) {
+	ctx := context.Background()
+	ctx = fplog.ContextWithLogger(ctx)
+
+	assert := assert.New(t)
+	hr := HTTPRequest{}
+	input := types.Input(map[string]interface{}{
+		"url":    "https://jsonplaceholder.typicode.com/posts",
+		"method": "post",
+		"body": `{
+			"userId": 1001,
+			"it": 1001,
+			"title": "Test 1001"
+		}`,
+		"request_timeout_ms": 3000,
+		"insecure":           false,
+		"ca_cert_pem":        "test",
+	})
+	_, err := hr.Run(ctx, input)
+	assert.NotNil(err, "no error found")
+	assert.Contains(err.Error(), "unknown authority")
 }
