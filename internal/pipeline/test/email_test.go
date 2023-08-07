1000dfa92aee6f1be95ec44c35f3a52dfc9f22cb
Subhajit Mondal <subhajit@turbot.com>
2023-08-07 21:29:22 +0530

    Update test

--- internal/pipeline/test/email_test.go
+++ internal/pipeline/test/email_test.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"testing"
 
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/gohcl"
 	"github.com/stretchr/testify/assert"
 	"github.com/turbot/flowpipe/internal/pipeline"
+	"github.com/zclconf/go-cty/cty"
 )
 
 func TestEmailStep(t *testing.T) {
@@ -17,7 +20,7 @@ func TestEmailStep(t *testing.T) {
 	assert.GreaterOrEqual(len(pipelines), 1, "wrong number of pipelines")
 
 	if pipelines["email"] == nil {
-		assert.Fail("query pipeline not found")
+		assert.Fail("email pipeline not found")
 		return
 	}
 
@@ -41,4 +44,62 @@ func TestEmailStep(t *testing.T) {
 	assert.Equal("Test email", inputs["subject"])
 	assert.Equal("This is a test email", inputs["body"])
 	assert.Equal("Flowpipe", inputs["sender_name"])
+}
+
+func TestEmailStepWithParam(t *testing.T) {
+	assert := assert.New(t)
+
+	pipelines, err := pipeline.LoadPipelines(context.TODO(), "./test_pipelines/email.fp")
+	assert.Nil(err, "error found")
+
+	assert.GreaterOrEqual(len(pipelines), 1, "wrong number of pipelines")
+
+	if pipelines["subscribe"] == nil {
+		assert.Fail("pipeline not found")
+		return
+	}
+
+	step := pipelines["subscribe"].GetStep("email.send_it")
+	if step == nil {
+		assert.Fail("email step not found")
+		return
+	}
+
+	inputs, err := step.GetInputs(nil)
+	if err != nil {
+		assert.Fail("error getting inputs")
+		return
+	}
+
+	assert.Contains(inputs["to"], "recipient@example.com")
+	assert.Equal("sender@example.com", inputs["from"])
+	assert.Equal("sendercredential", inputs["sender_credential"])
+	assert.Equal("smtp.example.com", inputs["host"])
+	assert.Equal("587", inputs["port"])
+	assert.Equal("You have been subscribed", inputs["subject"])
+
+	var output string
+	expr := pipelines["subscribe"].Steps[1].GetUnresolvedAttributes()["body"]
+
+	objectVal := cty.ObjectVal(map[string]cty.Value{
+		"echo": cty.ObjectVal(map[string]cty.Value{
+			"email_body": cty.ObjectVal(map[string]cty.Value{
+				"text": cty.StringVal("This is an email body"),
+			}),
+		}),
+	})
+
+	evalContext := &hcl.EvalContext{}
+	evalContext.Variables = map[string]cty.Value{}
+	evalContext.Variables["step"] = objectVal
+
+	diag := gohcl.DecodeExpression(expr, evalContext, &output)
+	if diag.HasErrors() {
+		assert.Fail("error decoding expression")
+		return
+	}
+	assert.Equal("This is an email body", output, "wrong output")
+
+	dependsOn := step.GetDependsOn()
+	assert.Contains(dependsOn, "echo.email_body")
 }
