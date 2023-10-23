b1f7b9a3fadc547cdf707564341fb1cca36e48ff
Victor Hadianto <victor@turbot.com>
2023-10-23 12:29:33 +0000

    Revert change to detect functions and containers in system load. I think we should lazy load this instead. This is breaking the integration test as well.

--- internal/service/manager/manager.go
+++ internal/service/manager/manager.go
@@ -2,7 +2,6 @@ package manager
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"os/signal"
 	"strings"
@@ -22,7 +21,6 @@ import (
 	"github.com/turbot/pipe-fittings/misc"
 	"github.com/turbot/pipe-fittings/modconfig"
 	"github.com/turbot/pipe-fittings/perr"
-	"github.com/turbot/pipe-fittings/schema"
 	"github.com/turbot/pipe-fittings/utils"
 	"github.com/turbot/pipe-fittings/workspace"
 )
@@ -150,19 +148,6 @@ func (m *Manager) Initialize() error {
 
 		pipelines = mod.ResourceMaps.Pipelines
 		triggers = mod.ResourceMaps.Triggers
-
-		// Return error if functions flag is not set and the pipeline has container or function steps
-		functionFlag := viper.GetBool(constants.ArgFunctions)
-		if !functionFlag {
-			// Check for container or function steps
-			for _, pipeline := range pipelines {
-				for _, step := range pipeline.Steps {
-					if step.GetType() == schema.BlockTypePipelineStepFunction || step.GetType() == schema.BlockTypeContainer {
-						return fmt.Errorf("cannot load pipelines with container or function steps. To use container or function please run flowpipe service start --functions.")
-					}
-				}
-			}
-		}
 
 		for _, dependendMode := range mod.ResourceMaps.Mods {
 			if dependendMode.Name() != mod.Name() {
