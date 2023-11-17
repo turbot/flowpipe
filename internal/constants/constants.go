58be5e8210ad4f124d97c6bab9141491529c12ae
kai <kai@turbot.com>
2023-11-17 15:30:19 +0000

    tidy

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -1,8 +1,8 @@
 package constants
 
 const (
-	DefaultServerPort = 7103
-	DefaultListen     = "localhost"
+	DefaultServerPort         = 7103
+	DefaultListen             = "localhost"
 	DefaultExecutionMode      = ExecutionModeAsynchronous
 	DefaultWaitRetry          = 60
 	ExecutionModeSynchronous  = "synchronous"
