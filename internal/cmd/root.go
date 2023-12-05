d3338aa54fd51b278f4927c5ff3273009770293f
kai <kai@turbot.com>
2023-12-05 12:21:51 +0000

    fix config-path

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -37,7 +37,7 @@ func rootCommand(ctx context.Context) *cobra.Command {
 		// Flowpipe API
 		AddPersistentStringFlag(constants.ArgHost, "", "API server host, including the port number").
 		AddPersistentBoolFlag(localconstants.ArgTlsInsecure, false, "Skip TLS verification").
-		AddStringFlag(constants.ArgConfigPath, localconstants.DefaultConfigPath, "Colon separated list of paths to search for workspace files, in order of decreasing precedence").
+		AddPersistentStringFlag(constants.ArgConfigPath, "", "Colon separated list of paths to search for workspace files, in order of decreasing precedence").
 		// Common (steampipe, flowpipe) flags
 		AddPersistentFilepathFlag(constants.ArgModLocation, cwd, "Path to the workspace working directory").
 		AddPersistentStringFlag(constants.ArgWorkspaceProfile, "default", "The workspace to use").
--- internal/cmdconfig/app_specific.go
+++ internal/cmdconfig/app_specific.go
@@ -3,10 +3,13 @@ package cmdconfig
 import (
 	"github.com/turbot/pipe-fittings/app_specific"
 	"github.com/turbot/pipe-fittings/cmdconfig"
+	"strings"
 )
 
 // SetAppSpecificConstants sets app specific constants defined in pipe-fittings
 func SetAppSpecificConstants() {
+	app_specific.DefaultConfigPath = strings.Join([]string{".", "~/.flowpipe/config"}, ":")
+
 	app_specific.AppName = "flowpipe"
 	// TODO unify version logic with steampipe and powerpipe
 	//app_specific.AppVersion
@@ -17,7 +20,7 @@ func SetAppSpecificConstants() {
 	app_specific.DefaultVarsFileName = "flowpipe.fpvars"
 	//app_specific.DefaultWorkspaceDatabase
 	app_specific.SetAppSpecificEnvVarKeys("FLOWPIPE_")
-	app_specific.EnvInputVarPrefix = "FP_VAR_"
+	app_specific.EnvInputVarPrefix = "P_VAR_"
 
 	app_specific.ConfigExtension = ".fpc"
 	app_specific.ModDataExtension = ".fp"
@@ -28,5 +31,4 @@ func SetAppSpecificConstants() {
 	app_specific.WorkspaceDataDir = ".flowpipe"
 	// set the command pre and post hooks
 	cmdconfig.CustomPreRunHook = preRunHook
-
 }
