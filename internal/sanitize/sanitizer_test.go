c672481032855651bedacc7ded6175cd1e90ae42
kai <kai@turbot.com>
2023-12-07 15:46:21 +0000

    update test

--- internal/sanitize/sanitizer_test.go
+++ internal/sanitize/sanitizer_test.go
@@ -27,6 +27,14 @@ func TestSanitizer_SanitizeString(t *testing.T) {
 			input: `{"password":"mypass12345"}`,
 			want:  `{"password":"` + RedactedStr + `"}`,
 		},
+		{
+			name: "full replacement github",
+			opts: SanitizerOptions{
+				ExcludePatterns: []string{"(?m)(ghp|gho|ghu|ghs|ghr)_[A-Za-z0-9_]{36}"},
+			},
+			input: `key = ghp_abcdfyocz0uxyzyO9Xn2Estui2kv12aaabgd`,
+			want:  `key = ` + RedactedStr,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
