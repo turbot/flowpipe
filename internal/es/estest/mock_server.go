7d3f06151faf58238c9c269b82886eed46788e33
Victor Hadianto <victor@turbot.com>
2023-12-07 04:29:17 +0000

    Added mock server for automated testing. Added test to ensur that empty array is returned as empty array (as opposed to nil).

---
+++ internal/es/estest/mock_server.go
@@ -0,0 +1,42 @@
+package estest
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func mockHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/special-case" {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "This is a special case"}`)) //nolint:errcheck // just a test case
+		return
+	}
+
+	// Default case: serve files from the directory
+	fileToReturn := filepath.Join("./test_data", r.URL.Path)
+
+	data, err := os.ReadFile(fileToReturn)
+	if err != nil {
+		http.Error(w, "File not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(data) //nolint:errcheck // just a test case
+}
+
+func StartServer() *http.Server {
+	server := &http.Server{Addr: ":7104", Handler: http.HandlerFunc(mockHandler)} //nolint:gosec // just a test case
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Could not listen on %s: %v\n", server.Addr, err)
+		}
+	}()
+
+	return server
+}
