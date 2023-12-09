ff4b990eafe9b65b4356a600d20ef14d0a2d3401
kai <kai@turbot.com>
2023-12-09 22:23:57 +0000

    Update WithServerConfig to map listen value

--- internal/service/manager/manager_option.go
+++ internal/service/manager/manager_option.go
@@ -17,10 +17,21 @@ func WithDocker() ManagerOption {
 
 func WithServerConfig(addr string, port int) ManagerOption {
 	return func(m *Manager) {
-		if addr == "local" {
-			addr = "localhost"
+
+		// map addr values as follows:
+		//		"local" -> "localhost"
+		//      "network" -> "" , i.e. listen on all addresses
+		switch addr {
+		case "local":
+			// listen on local host
+			m.HTTPAddress = "localhost"
+		case "network":
+			// listen on all addresses
+			addr = ""
+		default:
+			m.HTTPAddress = addr
+
 		}
-		m.HTTPAddress = addr
 		m.HTTPPort = port
 		m.startup |= startDocker | startES | startAPI | startScheduler
 	}
