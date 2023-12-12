3df358c15ea1cc586dcc2e96ac1342081de378d7
Subhajit Mondal <subhajit@turbot.com>
2023-12-12 20:40:19 +0530

    Add support to make the `container` step `timeout` attribute configurable (#497)

--- internal/primitive/container_test.go
+++ internal/primitive/container_test.go
@@ -35,7 +35,7 @@ func TestSimpleContainerStep(t *testing.T) {
 		schema.AttributeTypeImage:             "alpine:3.7",
 		schema.AttributeTypeCmd:               []interface{}{"sh", "-c", "echo 'Line 1'; echo 'Line 2'; echo 'Line 3'"},
 		schema.AttributeTypeEnv:               map[string]interface{}{"FOO": "bar"},
-		schema.AttributeTypeTimeout:           int64(120),
+		schema.AttributeTypeTimeout:           int64(120000),
 		schema.LabelName:                      "container_test",
 		schema.AttributeTypeMemory:            int64(128),
 		schema.AttributeTypeMemoryReservation: int64(64),
@@ -80,7 +80,7 @@ func TestContainerStepMissingImage(t *testing.T) {
 	input := modconfig.Input(map[string]interface{}{
 		schema.AttributeTypeCmd:     []interface{}{"echo", "hello world"},
 		schema.AttributeTypeEnv:     map[string]interface{}{"FOO": "bar"},
-		schema.AttributeTypeTimeout: int64(120),
+		schema.AttributeTypeTimeout: int64(120000),
 		schema.LabelName:            "container_test",
 	})
 
@@ -102,7 +102,7 @@ func TestContainerStepInvalidImage(t *testing.T) {
 		schema.AttributeTypeImage:   123,
 		schema.AttributeTypeCmd:     []interface{}{"echo", "hello world"},
 		schema.AttributeTypeEnv:     map[string]interface{}{"FOO": "bar"},
-		schema.AttributeTypeTimeout: int64(120),
+		schema.AttributeTypeTimeout: int64(120000),
 		schema.LabelName:            "container_test",
 	})
 
@@ -124,7 +124,7 @@ func TestContainerStepInvalidMemory(t *testing.T) {
 		schema.AttributeTypeImage:   "alpine:3.7",
 		schema.AttributeTypeCmd:     []interface{}{"echo", "hello world"},
 		schema.AttributeTypeEnv:     map[string]interface{}{"FOO": "bar"},
-		schema.AttributeTypeTimeout: int64(120),
+		schema.AttributeTypeTimeout: int64(120000),
 		schema.LabelName:            "container_test",
 		schema.AttributeTypeMemory:  int64(1),
 	})
@@ -148,7 +148,7 @@ func TestContainerStepInvalidCmd(t *testing.T) {
 		schema.AttributeTypeImage:   "alpine:3.7",
 		schema.AttributeTypeCmd:     "echo",
 		schema.AttributeTypeEnv:     map[string]interface{}{"FOO": "bar"},
-		schema.AttributeTypeTimeout: int64(120),
+		schema.AttributeTypeTimeout: int64(120000),
 		schema.LabelName:            "container_test",
 	})
 
@@ -170,7 +170,7 @@ func TestContainerStepInvalidEntrypoint(t *testing.T) {
 		schema.AttributeTypeImage:      "alpine:3.7",
 		schema.AttributeTypeEntryPoint: "echo",
 		schema.AttributeTypeEnv:        map[string]interface{}{"FOO": "bar"},
-		schema.AttributeTypeTimeout:    int64(120),
+		schema.AttributeTypeTimeout:    int64(120000),
 		schema.LabelName:               "container_test",
 	})
 
@@ -180,4 +180,50 @@ func TestContainerStepInvalidEntrypoint(t *testing.T) {
 	fpErr := err.(perr.ErrorModel)
 	assert.Equal("Container attribute 'entrypoint' must be an array of strings", fpErr.Detail)
 	assert.Equal(400, fpErr.Status)
+}
+
+func TestContainerStepTimeoutString(t *testing.T) {
+	ctx := context.Background()
+
+	assert := assert.New(t)
+	hr := Container{}
+
+	input := modconfig.Input(map[string]interface{}{
+		schema.AttributeTypeImage:             "alpine:3.7",
+		schema.AttributeTypeCmd:               []interface{}{"sh", "-c", "echo 'Line 1'; echo 'Line 2'; echo 'Line 3'"},
+		schema.AttributeTypeEnv:               map[string]interface{}{"FOO": "bar"},
+		schema.AttributeTypeTimeout:           "120s",
+		schema.LabelName:                      "container_test",
+		schema.AttributeTypeMemory:            int64(128),
+		schema.AttributeTypeMemoryReservation: int64(64),
+		schema.AttributeTypeMemorySwap:        int64(256),
+		schema.AttributeTypeMemorySwappiness:  int64(10),
+		schema.AttributeTypeReadOnly:          false,
+		schema.AttributeTypeUser:              "root",
+	})
+
+	output, err := hr.Run(ctx, input)
+	assert.Nil(err)
+	assert.Equal(0, len(output.Errors))
+	assert.NotNil(output.Get("container_id"))
+	assert.Equal(0, output.Get("exit_code"))
+	assert.Equal("Line 1\nLine 2\nLine 3\n", output.Get("stdout"))
+	assert.Equal("", output.Get("stderr"))
+
+	assert.NotNil(output.Get("lines"))
+
+	if _, ok := output.Get("lines").([]container.OutputLine); !ok {
+		assert.Fail("Expected lines to be []container.OutputLine")
+	}
+	lines := output.Get("lines").([]container.OutputLine)
+	assert.Equal(3, len(lines))
+
+	assert.Equal("stdout", lines[0].Stream)
+	assert.Equal("Line 1\n", lines[0].Line)
+
+	assert.Equal("stdout", lines[1].Stream)
+	assert.Equal("Line 2\n", lines[1].Line)
+
+	assert.Equal("stdout", lines[2].Stream)
+	assert.Equal("Line 3\n", lines[2].Line)
 }
