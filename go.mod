07510b6525800c1123cbb4159d32d82c3154ad5f
Graza <code@graza.io>
2023-12-18 12:48:40 +0000

    Added update check (#518)

--- go.mod
+++ go.mod
@@ -40,7 +40,7 @@ require (
 	github.com/denisss025/slog-watermill v0.1.0
 	github.com/logrusorgru/aurora v2.0.3+incompatible
 	github.com/turbot/flowpipe-sdk-go v0.1.0
-	github.com/turbot/pipe-fittings v0.1.0
+	github.com/turbot/pipe-fittings v0.1.1
 	github.com/turbot/steampipe-plugin-code v0.5.1
 )
 
--- internal/cmdconfig/app_specific.go
+++ internal/cmdconfig/app_specific.go
@@ -1,21 +1,25 @@
 package cmdconfig
 
 import (
+	"github.com/Masterminds/semver/v3"
+	"github.com/spf13/viper"
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/turbot/go-kit/files"
 	"github.com/turbot/pipe-fittings/app_specific"
 	"github.com/turbot/pipe-fittings/cmdconfig"
 	"github.com/turbot/pipe-fittings/error_helpers"
-	"os"
-	"path/filepath"
-	"strings"
 )
 
 // SetAppSpecificConstants sets app specific constants defined in pipe-fittings
 func SetAppSpecificConstants() {
 	app_specific.AppName = "flowpipe"
 
-	// TODO unify version logic with steampipe and powerpipe
-	//app_specific.AppVersion
+	// TODO unify version logic with steampipe
+	versionString := viper.GetString("main.version")
+	app_specific.AppVersion = semver.MustParse(versionString)
 
 	// set all app specific env var keys
 	app_specific.SetAppSpecificEnvVarKeys("FLOWPIPE_")
@@ -33,6 +37,11 @@ func SetAppSpecificConstants() {
 
 	// set the command pre and post hooks
 	cmdconfig.CustomPreRunHook = preRunHook
+	cmdconfig.CustomPostRunHook = postRunHook
+
+	// Version check
+	app_specific.VersionCheckHost = "hub.flowpipe.io"
+	app_specific.VersionCheckPath = "api/cli/version/latest"
 
 	// set the default install dir
 	defaultInstallDir, err := files.Tildefy("~/.flowpipe")
@@ -44,6 +53,15 @@ func SetAppSpecificConstants() {
 	// check whether install-dir env has been set - if so, respect it
 	if envInstallDir, ok := os.LookupEnv(app_specific.EnvInstallDir); ok {
 		globalConfigPath = filepath.Join(envInstallDir, "config")
+		app_specific.InstallDir = envInstallDir
+	} else {
+		/*
+			NOTE:
+			If InstallDir is settable outside of default & env var, need to add
+			the following code to end of initGlobalConfig in init.go
+			app_specific.InstallDir = viper.GetString(constants.ArgInstallDir) at end of
+		*/
+		app_specific.InstallDir = defaultInstallDir
 	}
 	app_specific.DefaultConfigPath = strings.Join([]string{".", globalConfigPath}, ":")
 }
