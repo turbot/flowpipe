45c5cca076a5209b4cf34873abd3738c2e987b78
Graza <code@graza.io>
2024-01-08 10:07:47 +0000

    refactor: added server output when server fails to start up (#553)

--- internal/cmd/server.go
+++ internal/cmd/server.go
@@ -3,8 +3,9 @@ package cmd
 import (
 	"fmt"
 	"github.com/turbot/flowpipe/internal/output"
+	"github.com/turbot/flowpipe/internal/types"
 	"net"
-	"strconv"
+	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -45,26 +46,32 @@ func serverCmd() *cobra.Command {
 func startServerFunc() func(cmd *cobra.Command, args []string) {
 	return func(cmd *cobra.Command, args []string) {
 		ctx := cmd.Context()
+		output.IsServerMode = true
 
 		// Check if the port is already in use
 		if isPortInUse(viper.GetInt(constants.ArgPort)) {
-			error_helpers.FailOnError(perr.InternalWithMessage("The designated port (" + strconv.Itoa(viper.GetInt(constants.ArgPort)) + ") is already in use"))
+			errMsg := fmt.Sprintf("the designated port (%d) is already in use", viper.GetInt(constants.ArgPort))
+			output.RenderServerOutput(ctx, types.NewServerOutputError(types.NewServerOutputPrefix(time.Now(), "flowpipe"), "unable to start server", fmt.Errorf(errMsg)))
+			error_helpers.FailOnError(perr.InternalWithMessage(errMsg))
 			return
 		}
 
 		outputMode := viper.GetString(constants.ArgOutput)
 		if outputMode == constants.OutputFormatJSON || outputMode == constants.OutputFormatYAML {
-			error_helpers.FailOnError(perr.BadRequestWithMessage("Currently '--output' is not supported for json or yaml"))
+			errMsg := "currently '--output' is not supported for json or yaml"
+			output.RenderServerOutput(ctx, types.NewServerOutputError(types.NewServerOutputPrefix(time.Now(), "flowpipe"), "unable to start server", fmt.Errorf(errMsg)))
+			error_helpers.FailOnError(perr.BadRequestWithMessage(errMsg))
 			return
 		}
-
-		output.IsServerMode = true
 
 		// start manager, passing server config
 		// (this will ensure manager starts API, ES, Scheduling and docker services
 		m, err := manager.NewManager(ctx,
 			manager.WithServerConfig(viper.GetString(constants.ArgListen), viper.GetInt(constants.ArgPort)),
 		).Start()
+		if err != nil {
+			output.RenderServerOutput(ctx, types.NewServerOutputError(types.NewServerOutputPrefix(time.Now(), "flowpipe"), "unable to start server", err))
+		}
 		error_helpers.FailOnError(err)
 
 		// Block until we receive a signal
