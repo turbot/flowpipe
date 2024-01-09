e4cfa5c0b7fa863804ffc6ffb25a461c9b498100
kai <kai@turbot.com>
2024-01-09 15:06:28 +0000

    update GetPrinter

--- internal/cmdconfig/cmd_output.go
+++
@@ -1,7 +0,0 @@
-package cmdconfig
-
-var CommandsWithTableOutput = []string{
-	"flowpipe.trigger.list",
-	"flowpipe.pipeline.list",
-	"flowpipe.process.list",
-}
--- internal/output/get_printer.go
+++ internal/output/get_printer.go
@@ -2,10 +2,13 @@ package output
 
 import (
 	"github.com/spf13/cobra"
-	localcmdconfig "github.com/turbot/flowpipe/internal/cmdconfig"
 	"github.com/turbot/pipe-fittings/printers"
 )
 
 func GetPrinter[T any](cmd *cobra.Command) (printers.ResourcePrinter[T], error) {
-	return printers.GetPrinter[T](cmd, printers.WithTableCommands(localcmdconfig.CommandsWithTableOutput))
+	return printers.GetPrinter[T](cmd, printers.WithTableCommands([]string{
+		"flowpipe.trigger.list",
+		"flowpipe.pipeline.list",
+		"flowpipe.process.list",
+	}))
 }
