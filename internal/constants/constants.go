1fd918812338d0cefdd15191ae8e880188d07805
Graza <code@graza.io>
2024-02-07 14:14:10 +0000

    feat: default integration/notifier files

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -14,3 +14,15 @@ const (
 
 	MaxScanSize = bufio.MaxScanTokenSize * 40
 )
+
+const DefaultFlowpipeIntegrationContent = `
+integration "webform" "default" {}
+`
+
+const DefaultFlowpipeNotifierContent = `
+notifier "default" {
+  integration "default" {
+    base = integration.webform.default 
+  }
+}
+`
