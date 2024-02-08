73af7c653bbde1fbfcaffcd9731499700462c3cc
Graza <code@graza.io>
2024-02-08 07:22:06 +0000

    refactor: removed default integration/notifier files now in code (#641)

--- internal/cmdconfig/init.go
+++ internal/cmdconfig/init.go
@@ -3,15 +3,10 @@ package cmdconfig
 import (
 	"maps"
 	"os"
-	"path/filepath"
-	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	con "github.com/turbot/flowpipe/internal/constants"
 	"github.com/turbot/flowpipe/internal/log"
-	"github.com/turbot/go-kit/files"
-	"github.com/turbot/pipe-fittings/app_specific"
 	"github.com/turbot/pipe-fittings/cmdconfig"
 	"github.com/turbot/pipe-fittings/constants"
 	"github.com/turbot/pipe-fittings/error_helpers"
@@ -21,9 +16,6 @@ import (
 )
 
 func initGlobalConfig() *flowpipeconfig.FlowpipeConfig {
-	// check defaults prior to loading into workspace
-	defaultIntegrationNotifierFiles()
-
 	// load workspace profile from the configured install dir
 	loader, err := cmdconfig.GetWorkspaceProfileLoader[*modconfig.FlowpipeWorkspaceProfile]()
 	error_helpers.FailOnError(err)
@@ -84,27 +76,4 @@ func getConfigDefaults(cmd *cobra.Command) map[string]any {
 		maps.Copy(res, cmdSpecificDefaults)
 	}
 	return res
-}
-
-func defaultIntegrationNotifierFiles() {
-	installPath := app_specific.DefaultInstallDir
-	// configPath := strings.Split(app_specific.DefaultConfigPath, ":")[len(strings.Split(app_specific.DefaultConfigPath, ":"))-1]
-	configPath := filepath.Join(installPath, "config")
-	internalStateFile := filepath.Join(installPath, "internal", ".webform_initialized")
-	integrationFile := filepath.Join(configPath, "integrations.fpc")
-	notifierFile := filepath.Join(configPath, "notifiers.fpc")
-	if !files.FileExists(internalStateFile) {
-		if !files.FileExists(integrationFile) {
-			//nolint: gosec // this file is safe to be read by all users
-			_ = os.WriteFile(integrationFile, []byte(con.DefaultFlowpipeIntegrationContent), 0755)
-		}
-		if !files.FileExists(notifierFile) {
-			//nolint: gosec // this file is safe to be read by all users
-			_ = os.WriteFile(notifierFile, []byte(con.DefaultFlowpipeNotifierContent), 0755)
-		}
-
-		ts := time.Now().Format(time.RFC3339)
-		//nolint: gosec // this file is safe to be read by all users
-		_ = os.WriteFile(internalStateFile, []byte(ts), 0755)
-	}
 }
