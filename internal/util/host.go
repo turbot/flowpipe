27daeee368f932fd35175878cddbc783af5fa8ee
Victor Hadianto <victor@turbot.com>
2024-02-22 10:08:04 +0000

    wip

--- internal/util/host.go
+++ internal/util/host.go
@@ -3,6 +3,8 @@ package util
 import (
 	"fmt"
 	"net/url"
+	"os"
+	"strings"
 
 	"github.com/spf13/viper"
 	localconstants "github.com/turbot/flowpipe/internal/constants"
@@ -33,6 +35,12 @@ func GetBaseUrl() string {
 }
 
 func GetWebformUrl(execId string, pExecId string, sExecId string) (string, error) {
+
+	if strings.HasPrefix(os.Getenv("RUN_MODE"), "TEST") {
+		// in test env there's no global salt
+		return "http://localhost:7103/webform/input/" + sExecId + "/abcdefg", nil
+	}
+
 	baseUrl := GetBaseUrl()
 	last8 := db.MapStepExecutionID(execId, pExecId, sExecId)
 
