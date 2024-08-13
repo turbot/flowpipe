711cdf585fadfc5eca0edcd989e990b44e21a164
Victor Hadianto <victor@turbot.com>
2024-08-13 05:02:33 +0000

    wip

--- internal/es/estest/default_mod/functions/sleep/index.js
+++ internal/es/estest/default_mod/functions/sleep/index.js
@@ -7,7 +7,8 @@ exports.handler = async (event, context) => {
   console.log(`Initial log at ${currentTime}`);
 
   // Loop for 10 minutes (600 seconds) with a 2-second interval
-  for (let i = 0; i < 300; i++) {  // 300 iterations * 2 seconds = 600 seconds = 10 minutes
+  // for (let i = 0; i < 300; i++) {  // 300 iterations * 2 seconds = 600 seconds = 10 minutes
+  for (let i = 0; i < 1; i++) {
     console.log(`Heartbeat log at ${moment().format('YYYY-MM-DD HH:mm:ss')}`);
     await new Promise(resolve => setTimeout(resolve, 2000));  // wait for 2 seconds
   }
@@ -15,7 +16,7 @@ exports.handler = async (event, context) => {
   const response = {
     statusCode: 300,
     body: {
-      message: `Hola, World! The current time is ${currentTime}. From ${event.user.name} with age: ${event.user.age}. Not nested: ${event.notNested}.`,
+      message: "test",
       event,
       env: process.env,
       context
