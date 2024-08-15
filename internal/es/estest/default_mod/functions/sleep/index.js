33e9a8815d3fa8a003a4331cb5f1635de81db101
Omero Saienni <omero@turbot.com>
2024-08-15 12:45:28 +1200

    Retry  without formatting

--- internal/es/estest/default_mod/functions/sleep/index.js
+++ internal/es/estest/default_mod/functions/sleep/index.js
@@ -1,15 +1,15 @@
-const moment = require("moment");
+const moment = require('moment');
 
 exports.handler = async (event, context) => {
-  const currentTime = moment().format("YYYY-MM-DD HH:mm:ss");
+  const currentTime = moment().format('YYYY-MM-DD HH:mm:ss');
 
   // Printing initial log
   console.log(`Initial log at ${currentTime}`);
 
   // Loop for 10 minutes (600 seconds) with a 2-second interval
-  for (let i = 0; i < 300; i++) { // 300 iterations * 2 seconds = 600 seconds = 10 minutes
-    console.log(`Heartbeat log at ${moment().format("YYYY-MM-DD HH:mm:ss")}`);
-    await new Promise((resolve) => setTimeout(resolve, 2000)); // wait for 2 seconds
+  for (let i = 0; i < 300; i++) {  // 300 iterations * 2 seconds = 600 seconds = 10 minutes
+    console.log(`Heartbeat log at ${moment().format('YYYY-MM-DD HH:mm:ss')}`);
+    await new Promise(resolve => setTimeout(resolve, 2000));  // wait for 2 seconds
   }
 
   const response = {
@@ -18,7 +18,7 @@ exports.handler = async (event, context) => {
       message: `Hola, World! The current time is ${currentTime}. From ${event.user.name} with age: ${event.user.age}. Not nested: ${event.notNested}.`,
       event,
       env: process.env,
-      context,
+      context
     },
   };
   return response;
